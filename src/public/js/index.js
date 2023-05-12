c9aec68383632d0442790b948d530b39d421beef
Ezequiel Miceli <ezee87h@gmail.com>
2023-05-12 00:55:12 -0300

    Se agrega boostrap

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -21,7 +21,22 @@ socketClient.on("arrayProducts", (array) => {
   console.log(array);
   let infoProducts = "";
   array.forEach((p) => {
-    infoProducts += `Title: ${p.title} | Description: ${p.description} | Price: $${p.price} | Category: ${p.category} | Stock: ${p.stock} | Code: ${p.code} |  <br>`;
+
+    infoProducts += `
+    <div class="d-flex justify-content-center align-items-center">
+    <div class="text-center border border-info rounded my-3 py-2 px-3">
+      <p>
+      <h3>Nuevo producto</h3>
+        <b>${p.title}</b></br>
+        <u>Descripcion:</u> ${p.description}</br>
+        <u> Precio:</u> $${p.price} </br>
+        <u>Categoria:</u> ${p.category}</br>
+        <u>Stock:</u> ${p.stock}</br>
+        <u>Code:</u> ${p.code}</br>
+      </p>
+    </div>
+  </div>
+  `;
   });
   products.innerHTML = infoProducts;
 });
